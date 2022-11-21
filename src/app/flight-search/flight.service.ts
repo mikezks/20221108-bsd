0dcf8fd50867562a9c751239417deb31cd53fa96
mikezks <23002661+mikezks@users.noreply.github.com>
2022-11-21 09:41:36 +0100

    feat: di use factory

--- src/app/flight-search/flight.service.ts
+++ src/app/flight-search/flight.service.ts
@@ -1,13 +1,21 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Flight } from '../entities/flight';
 import { DefaultFlightService } from './default-flight.service';
 import { DummyFlightService } from './dummy-flight.service';
 
 @Injectable({
   providedIn: 'root',
-  useClass: DefaultFlightService
+  useFactory: (http: HttpClient) => {
+    if (environment.useFlightService === 'default') {
+      return new DefaultFlightService(http);
+    } else {
+      return new DummyFlightService();
+    }
+  },
+  deps: [HttpClient]
 })
 export abstract class FlightService {
 
